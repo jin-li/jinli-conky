920dfdedf2ce9881321500f82d0b9680ab545ff6
jin-li <lijin.thu@outlook.com>
2025-09-16 23:01:32 +0200

    update start script to generate autostart entry

--- start.sh
+++ start.sh
@@ -8,6 +8,19 @@ cd $(dirname $0)
 # create a new config if there is none
 [[ -f jinli-config.lua ]] || cp jinli-config.example.lua jinli-config.lua
 
+# clear the old autostart entry (if any) and create a new one
+[[ -f conky-start.desktop ]] && rm conky-start.desktop
+cat > conky-start.desktop <<EOL
+[Desktop Entry]
+Type=Application
+Name=jinli-conky
+Comment=Conky, a system monitor
+Exec=$(pwd)/start.sh
+Icon=utilities-system-monitor
+Terminal=false
+Categories=Utility;System;
+EOL
+echo "Created autostart entry at $(pwd)/conky-start.desktop. Copy it to ~/.config/autostart to autostart conky at login."
 
 if [[ " $@ " =~ " --loop " ]]; then
   while true; do
